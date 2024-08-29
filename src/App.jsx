d7db552ec3fdf47254d8f5d0eaa7c7bb7740fcfa
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-08-29 13:44:02 +0900

    Init: 초기 환경 설정 구성 - 필요한 패키지 설치 완료 - router 연결 - page 폴더에 로그인, 홈, 회원가입 추가 - utils 폴더에 throttle과 debouce 그리고 router 관련 함수 추가

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import router from '@/router';
+import { RouterProvider } from 'react-router-dom';
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/pageTemplate.jsx
@@ -0,0 +1,12 @@
+import ComponentTemplate from '@/components/componentTemplate';
+
+const PageTemplate = () => {
+  return (
+    <>
+      <ComponentTemplate />
+      <h1>페이지 템플릿</h1>
+    </>
+  );
+};
+
+export default PageTemplate;
