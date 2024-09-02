5668e2c5a7d2e95a56402c63e013ff5b8bd6f91f
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-09-03 00:34:00 +0900

    Modify: input test를 위한 메인 페이지 원래대로 복원

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,27 +1,7 @@
-import AppInput from '@/components/AppInput/AppInput';
-import { throttle } from '@/utils';
-import { useState } from 'react';
-
 function Home() {
-  const [values, setValues] = useState('');
-
-  const handleChange = throttle((e) => {
-    setValues(e.target.value);
-    console.log(e.target.value);
-  });
-
   return (
     <main>
       <h1>메인페이지</h1>
-      <AppInput
-        label={'아이디'}
-        labelHidden={false}
-        type={'password'}
-        name={'아이디'}
-        defaultValue={values}
-        placeholder={'아이디 입력'}
-        onChange={handleChange}
-      />
     </main>
   );
 }
