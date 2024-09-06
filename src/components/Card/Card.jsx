54247247aeba10abef576add6aac2efb7843a1e7
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-09-07 00:51:45 +0900

    feat: 카드 컴포넌트 마크업 및 rankCard 스타일링 완료
    - 각각의 사이즈에 맞게 처리 완료

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,41 +1,64 @@
 import { Link } from 'react-router-dom';
 import MarkerFill from '@/assets/svg/marker-fill.svg?react';
 import S from './Card.module.css';
+import ToggleBtn from '../ToggleBtn/ToggleBtn';
 
-function ListCard({ type = 'rank' }) {
+function Card({ type = 'rank', fullSize = false }) {
+  const rankStyled = type == 'rank' && fullSize ? S.rankCardFull : S.rankCard;
   const placeInfo =
     type === 'rank' ? (
-      <article>
-        <div>
-          <p>
-            <span>1.</span>아쿠아 플래넷
-          </p>
+      <article className={rankStyled}>
+        <img
+          className={S.placePic}
+          src="/testingPic(delete-later).png"
+          alt=""
+        />
+        <article className={S.cardHeader}>
+          <div className={S.userInfos}>
+            <img src="/testUserPic(delete-later).png" alt="" />
+            <p>사용자</p>
+          </div>
+          <ToggleBtn Bookmark />
+        </article>
+        <article className={S.places}>
+          <div className={S.placeInfos}>
+            <p>
+              <span>1. </span>아쿠아 플래넷
+            </p>
+            <div className={S.placePosition}>
+              <MarkerFill className={S.marker} />
+              <span>서울, 도봉구</span>
+            </div>
+          </div>
+          <div className={S.heartWrapper}>
+            <span>556</span>
+            <ToggleBtn />
+          </div>
+        </article>
+      </article>
+    ) : (
+      <article className={S.listCard}>
+        <img
+          className={S.placePic}
+          src="/testingPic(delete-later).png"
+          alt=""
+        />
+        <div className={S.cardHeader}>
+          <ToggleBtn Bookmark />
+        </div>
+        <p>아쿠아 플래넷</p>
+        <div className={S.placeInfos}>
           <span>
             <MarkerFill />
             서울, 도봉구
           </span>
         </div>
-        <div>
-          <span>556</span>
-          <button>❤</button>
-        </div>
-      </article>
-    ) : (
-      <article>
-        <p>아쿠아 플래넷</p>
-        <span>
-          <MarkerFill />
-          서울, 도봉구
-        </span>
       </article>
     );
   return (
     <article className={S.component}>
-      <Link to={''}>
-        <img src="/testingPic(delete-later).png" alt="" />
-        {placeInfo}
-      </Link>
+      <Link to={''}>{placeInfo}</Link>
     </article>
   );
 }
-export default ListCard;
+export default Card;
