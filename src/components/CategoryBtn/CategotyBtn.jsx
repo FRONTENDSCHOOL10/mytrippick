9213c4bd90141038be002ff9ee0d7fe778c9076c
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-09-08 00:48:45 +0900

    feat: 카테고리 버튼 프롭타입스 설정

--- src/components/CategoryBtn/CategotyBtn.jsx
+++ src/components/CategoryBtn/CategotyBtn.jsx
@@ -1,5 +1,12 @@
 import { useId } from 'react';
 import S from './CategoryBtn.module.css';
+import { string, bool, func } from 'prop-types';
+
+CategoryBtn.propTypes = {
+  label: string.isRequired,
+  checked: bool,
+  onChecked: func,
+};
 
 function CategoryBtn({ label, checked = false, onChecked }) {
   const id = useId();
