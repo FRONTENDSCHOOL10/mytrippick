abb9cf802cb2bcfeed3f68dc4d9a38467137faee
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-09-08 00:32:12 +0900

    feat: 카테고리 컴포넌트 기본 마크업 및 스타일링 진행
    - 디자인의 주어진 대로 마크업 진행
    - 선택이 되었을때와 되지 않았을때를 구분함

--- src/components/CategoryBtn/CategotyBtn.jsx
+++ src/components/CategoryBtn/CategotyBtn.jsx
@@ -1,12 +1,19 @@
 import { useId } from 'react';
 import S from './CategoryBtn.module.css';
 
-function CategoryBtn({ label }) {
+function CategoryBtn({ label, checked = false, onChecked }) {
   const id = useId();
   return (
     <div className={S.component}>
+      <input
+        id={id}
+        type="radio"
+        name="category"
+        value={label}
+        defaultChecked={checked}
+        onClick={onChecked}
+      />
       <label htmlFor={id}>{label}</label>
-      <input id={id} type="radio" name="category" value={label} />
     </div>
   );
 }
