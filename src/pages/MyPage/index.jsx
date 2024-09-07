fc3c79bb48e047bcba25f704bf38f59d04ab1754
rewind122 <luvyoudear12@gmail.com>
2024-09-08 02:45:15 +0900

    Feat: 마이페이지 마크업 및 스타일링 수정
    - 확인을 위해 router에 마이페이지 추가
    - 마크업 구조 일부 수정, 버튼 클릭 이벤트 추가
    - 구조 수정에 따른 css 수정

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -3,7 +3,16 @@ import S from './MyPage.module.css';
 import Settings from '@/assets/svg/settings.svg?react';
 
 function MyPage() {
-  const noPost = false; // 임시 조건 처리
+  const noPost = false; // 임시 조건 처리, 배열 데이터의 길이가 0이면 true가 되는 식으로...
+
+  const handleClick = (e) => {
+    if (e.currentTarget.innerHTML === '로그아웃') {
+      location.href = '/'; // replace로 뒤로가기 안 먹히게 하는 게 나을지...?
+      // 로그아웃 처리도 해주기
+    } else {
+      location.href = '/게시글작성'; // 게시글 작성 페이지
+    }
+  };
 
   return (
     <>
@@ -21,9 +30,10 @@ function MyPage() {
             </p>
           </div>
         </div>
-        <button>
+        <a href="/회원정보수정">
+          {/* 회원 정보 수정 페이지로 이동 */}
           <Settings />
-        </button>
+        </a>
       </section>
       <section>
         <h2 className={`headline4 ${S.visitedPlace}`}>내가 방문한 여행지</h2>
@@ -33,24 +43,45 @@ function MyPage() {
               아직 여행지 기록이 없네요. <br />
               멋진 여행을 다녀오셨다면, 첫 기록을 남겨보세요!
             </p>
-            <CommonBtn small>여행 기록 작성하러 가기</CommonBtn>
+            <CommonBtn small onClick={handleClick}>
+              여행 기록 작성하러 가기
+            </CommonBtn>
+            {/* 클릭 시 게시글 작성 페이지로 이동 */}
           </div>
         ) : (
           <div className={S.postContainer}>
-            {/* 이 안에 리스트렌더링 */}
-            {/* 아래는 확인용 임시 이미지 */}
-            <img src="https://placehold.co/220x310" alt="" />
-            <img src="https://placehold.co/220x310" alt="" />
-            <img src="https://placehold.co/220x310" alt="" />
-            <img src="https://placehold.co/220x310" alt="" />
-            <img src="https://placehold.co/220x310" alt="" />
-            <img src="https://placehold.co/220x310" alt="" />
-            <img src="https://placehold.co/220x310" alt="" />
+            {/* 이 안에 리스트렌더링*/}
+            {/* 아래는 확인용 임시 이미지 목록 */}
+            {/* 링크는 각 게시글 상세페이지로 연결되도록, 이미지 대체텍스트는 장소 데이터를 넣기? */}
+            <a href="/">
+              <img src="https://placehold.co/220x310" alt="" />
+            </a>
+            <a href="/">
+              <img src="https://placehold.co/220x310" alt="" />
+            </a>
+            <a href="/">
+              <img src="https://placehold.co/220x310" alt="" />
+            </a>
+            <a href="/">
+              <img src="https://placehold.co/220x310" alt="" />
+            </a>
+            <a href="/">
+              <img src="https://placehold.co/220x310" alt="" />
+            </a>
+            <a href="/">
+              <img src="https://placehold.co/220x310" alt="" />
+            </a>
+            <a href="/">
+              <img src="https://placehold.co/220x310" alt="" />
+            </a>
           </div>
         )}
       </section>
       <section className={S.logout}>
-        <CommonBtn small>로그아웃</CommonBtn>
+        <CommonBtn small onClick={handleClick}>
+          로그아웃
+        </CommonBtn>
+        {/* 로그아웃 후 메인페이지로 이동하도록? */}
       </section>
     </>
   );
