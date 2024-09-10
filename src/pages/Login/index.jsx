f359c6c065c277f95f6a3f82878c8d907aeec449
SohnSW <irctivl@gmail.com>
2024-09-10 15:09:25 +0900

    feat: 로그인페이지 초기구성

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,62 @@
+import { useState } from 'react';
+import AppInput from '@/components/AppInput/AppInput';
+import CommonBtn from '@/components/CommonBtn/CommonBtn';
+import S from './Login.module.css';
+import { throttle } from '@/utils';
+
 function Login() {
-  return <h1>로그인 페이지</h1>;
+  const [formDatas, setFormDatas] = useState(() => {
+    return {
+      email: '',
+      password: '',
+      confirmPassWord: '',
+      nickName: '',
+    };
+  });
+
+  const handleFormDatasChange = throttle((e) => {
+    const targets = e.target.value;
+    console.log(targets);
+  });
+
+  return (
+    <section className={S.component}>
+      <h1 className="headline1">로그인</h1>
+      <form action="">
+        <article className="inputContainer">
+          <AppInput
+            label={'이메일 주소'}
+            labelHidden={false}
+            type={'email'}
+            name={'email'}
+            placeholder={'ID@example.com'}
+            defaultValue={formDatas.email}
+            onChange={handleFormDatasChange}
+          />
+          <span className="caption"></span>
+        </article>
+
+        <article className="inputContainer">
+          <AppInput
+            label={'비밀번호'}
+            labelHidden={false}
+            type={'password'}
+            name={'password'}
+            placeholder={'비밀번호를 입력해주세요.'}
+            defaultValue={formDatas.password}
+            onChange={handleFormDatasChange}
+          />
+          <span className="caption">
+            영문, 숫자, 특수문자 중 2가지 조합 8~15자
+          </span>
+        </article>
+
+        <CommonBtn submit={true} disabled={true} small={false} fill={true}>
+          로그인
+        </CommonBtn>
+      </form>
+    </section>
+  );
 }
 
 export default Login;
