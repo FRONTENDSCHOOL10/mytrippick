34810c644eefcdfd01b2ae4fc9483e39f0cb8da6
rewind122 <luvyoudear12@gmail.com>
2024-09-10 22:43:49 +0900

    Feat: 마이페이지 기능 추가
    - pocketbase 데이터 가져와서 렌더링
    - ProfileBox, LoginModal 컴포넌트 분리

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,48 +1,71 @@
+import getPbImageURL from '@/api/getPbImageURL';
+import pb from '@/api/pb';
 import CommonBtn from '@/components/CommonBtn/CommonBtn';
+import { useEffect, useState } from 'react';
 import S from './MyPage.module.css';
-import Settings from '@/assets/svg/settings.svg?react';
+import ProfileBox from './ProfileBox';
+import LoginModal from './LoginModal';
 
 function MyPage() {
-  const noPost = false; // 임시 조건 처리, 배열 데이터의 길이가 0이면 true가 되는 식으로...
+  // 상태
+  const [profileData, setProfileData] = useState({});
+  const [postList, setPostList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [, setError] = useState(null);
 
-  const handleClickProfile = (e) => {
-    console.log(e.currentTarget.src);
+  // 임시 설정(추후 수정 예정)
+  const currentUserId = 'tpht86ygsyb1dqv'; // 현재 로그인한 유저 id를 가져오도록
+  let isLoggedIn = true; // 로그인 상태 확인 -> useGlobalStore에서 가져올 수 있도록?
 
-    // 누르면 프사 크게 볼 수 있는 기능...
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // 데이터 가져오기
+        const record = await pb.collection('users').getOne(currentUserId);
+        setProfileData(record);
+
+        const currentUserPosts = await pb.collection('posts').getList(1, 50, {
+          filter: `userId = "${currentUserId}"`,
+          sort: '-created',
+        });
+        setPostList(currentUserPosts.items);
+        setIsLoading(false);
+      } catch (error) {
+        // 에러 처리
+        setError('데이터를 불러오는 중 오류가 발생했습니다.');
+        console.error('Error fetching data:', error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const noPost = postList.length === 0;
+
   const handleClickNewPost = () => {
     location.href = '/게시글작성'; // 게시글 작성 페이지로 이동
   };
   const handleClickLogout = () => {
-    location.href = '/'; // replace로 뒤로가기 안 먹히게 하는 게 나을지?
-    // 로그아웃 처리도 해주기
+    location.replace('/'); // 메인 페이지로 이동
+    isLoggedIn = false; // 임시 로그아웃 처리
   };
+  const handleLogin = () => {
+    location.href = '/login';
+  };
+
+  if (isLoading) {
+    return <p>로딩 중입니다...</p>;
+  }
+
+  if (!isLoggedIn) {
+    return <LoginModal isLoggedIn={isLoggedIn} onLogin={handleLogin} />;
+  }
 
   return (
     <>
       <h1 className="a11yHidden">마이페이지</h1>
-      <section className={S.profileContainer}>
-        <div className={S.profile} role="group">
-          <img
-            src="https://placehold.co/100"
-            alt="닉네임 프로필"
-            onClick={handleClickProfile}
-          />
-          <div className={S.profileText} role="group">
-            <h2 className="headline4">
-              닉네임최대닉네임최대닉네임최대닉네임최대
-            </h2>
-            <p>
-              소개글최대소개글최대소개글최대소개글최대소개글최대소개글최대소개글최대
-              소개글최대
-            </p>
-          </div>
-        </div>
-        <a href="/회원정보수정">
-          {/* 회원 정보 수정 페이지로 이동 */}
-          <Settings />
-        </a>
-      </section>
+      <ProfileBox userData={profileData} />
       <section>
         <h2 className={`headline4 ${S.visitedPlace}`}>내가 방문한 여행지</h2>
         {noPost ? (
@@ -57,32 +80,22 @@ function MyPage() {
             {/* 클릭 시 게시글 작성 페이지로 이동 */}
           </div>
         ) : (
-          <div className={S.postContainer}>
-            {/* 이 안에 리스트렌더링*/}
-            {/* 아래는 확인용 임시 이미지 목록 */}
-            {/* 링크는 각 게시글 상세페이지로 연결되도록, 이미지 대체텍스트는 장소 데이터를 넣기? */}
-            <a href="/">
-              <img src="https://placehold.co/220x310" alt="" />
-            </a>
-            <a href="/">
-              <img src="https://placehold.co/220x310" alt="" />
-            </a>
-            <a href="/">
-              <img src="https://placehold.co/220x310" alt="" />
-            </a>
-            <a href="/">
-              <img src="https://placehold.co/220x310" alt="" />
-            </a>
-            <a href="/">
-              <img src="https://placehold.co/220x310" alt="" />
-            </a>
-            <a href="/">
-              <img src="https://placehold.co/220x310" alt="" />
-            </a>
-            <a href="/">
-              <img src="https://placehold.co/220x310" alt="" />
-            </a>
-          </div>
+          <ul className={S.postContainer}>
+            {postList.map((item) => {
+              return (
+                <li key={item.id}>
+                  <a href="/">
+                    {/* 링크는 각 게시글 상세페이지로 연결되도록 추후 수정 */}
+                    <img
+                      src={getPbImageURL(item)}
+                      alt={item.placeName}
+                      title={item.placeName}
+                    />
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
         )}
       </section>
       <section className={S.logout}>
