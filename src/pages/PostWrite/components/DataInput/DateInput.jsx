5f1e5dd47c30226a81f4f45de211f0eb26393a80
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-09-11 16:44:28 +0900

    feat: 데이트 인풋 컴포넌트 작성
    - datePicker 패키지와 date-fns 패키지 설치
    - DateInput 마크업과 스타일링 진행

--- src/pages/PostWrite/components/DataInput/DateInput.jsx
+++ src/pages/PostWrite/components/DataInput/DateInput.jsx
@@ -1,20 +1,38 @@
-import { useState, useId } from 'react';
+import { useId, forwardRef } from 'react';
+import { string, func } from 'prop-types';
 import DatePicker from 'react-datepicker';
+import ko from 'date-fns/locale/ko';
 import 'react-datepicker/dist/react-datepicker.css';
-import Calender from '@/assets/svg/calander.svg?react';
+import Calendar from '@/assets/svg/calander.svg?react';
 import S from './DateInput.module.css';
+import usePostDateStore from '@/stores/usePostDateStore';
 
-function CustomInput({ value, onClick }) {
-  return (
-    <>
-      <input type="text" />
-      <Calender className={S.calendarIcon} />
-    </>
-  );
-}
+const CustomInput = forwardRef(({ value, onClick }, ref) => (
+  <div style={{ position: 'relative' }}>
+    <input
+      type="text"
+      value={value}
+      onClick={onClick}
+      onFocus={onClick}
+      ref={ref}
+      placeholder="날짜를 선택해주세요"
+      readOnly
+    />
+    <Calendar
+      style={{
+        position: 'absolute',
+        right: '10px',
+        top: '50%',
+        transform: 'translateY(-50%)',
+        pointerEvents: 'none',
+      }}
+    />
+  </div>
+));
 
 function DateInput() {
-  const [startDate, setStartDate] = useState(new Date());
+  const { date, setDate } = usePostDateStore();
+
   const id = useId();
 
   return (
@@ -24,13 +42,24 @@ function DateInput() {
       </label>
       <DatePicker
         id={id}
+        selected={date}
+        onChange={(date) => setDate(date)}
         dateFormat="yyyy.MM.dd"
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-        customInput={<CustomInput />}
+        locale={ko}
+        customInput={
+          <CustomInput value={date ? date.toLocaleDateString('ko-KR') : ''} />
+        }
       />
     </article>
   );
 }
+
+CustomInput.displayName = 'CustomInput';
+
+CustomInput.propTypes = {
+  value: string,
+  onClick: func,
+  onChange: func,
+};
 
 export default DateInput;
