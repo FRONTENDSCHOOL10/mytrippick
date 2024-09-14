35241fb981a59adfeff16fa1d3da22e382079f13
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-09-14 22:26:43 +0900

    feat: 장소 선택 모달 작성 완료 및 기능 추가
    - 카카오맵을 통해 데이터 가져옴
    - 사용자 입력시 각각 요소에 일치하는 데이터의 리스트 출력
    - 리스트 클릭 시 장소 선택됨과 동시에 모달창에서 아웃
    - 상태는 zustand를 사용해서 관리

--- src/pages/PostWrite/index.jsx
+++ src/pages/PostWrite/index.jsx
@@ -1,4 +1,4 @@
-import AppInput from '@/components/AppInput/AppInput';
+import { useState } from 'react';
 import CommonBtn from '@/components/CommonBtn/CommonBtn';
 import AppTextArea from '@/components/AppTextArea/AppTextArea';
 import CategoryBtnList from './components/CategoryBtnList/CategotyBtnList';
@@ -6,25 +6,33 @@ import UploadImage from './components/UploadImage/UploadImage';
 import DateInput from './components/DataInput/DateInput';
 import Search from '@/assets/svg/search.svg?react';
 import S from './PostWrite.module.css';
+import PlaceSearchModal from './components/PlaceSearchModal/PlaceSearchModal';
+import usePlaceDateStore from '@/stores/usePlaceDataStore';
 
 function PostWrite() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { placeName, placeAddress, placeLatLong } = usePlaceDateStore();
+
+  console.log(placeName, placeAddress, placeLatLong);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <section className={S.component}>
       <UploadImage />
       <article className={S.searchPlaceBtnWrapper}>
-        <h3 className="label">방문한 날짜</h3>
-        <button className={S.searchPlaceBtn} type="button">
-          여행지를 선택해주세요
+        <h3 className="label">여행지명</h3>
+        <button className={S.searchPlaceBtn} type="button" onClick={openModal}>
+          {placeName === null ? '여행지를 선택해주세요' : placeName}
           <Search aria-label="돋보기 버튼 아이콘" />
         </button>
       </article>
-      <AppInput
-        label={'여행지 주소'}
-        labelHidden={false}
-        type={'text'}
-        name={'placeAddress'}
-        placeholder={'여행지 주소를 선택해주세요'}
-      />
       <DateInput />
       <CategoryBtnList />
       <AppTextArea
@@ -36,6 +44,9 @@ function PostWrite() {
       <CommonBtn submit={true} disabled={true} fill={true}>
         등록
       </CommonBtn>
+
+      {isModalOpen && <PlaceSearchModal closeModal={closeModal} />}
+      {/* <PlaceSearchModal closeModal={closeModal} /> */}
     </section>
   );
 }
