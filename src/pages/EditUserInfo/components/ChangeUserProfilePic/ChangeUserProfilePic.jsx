3df7957130c1b8fc8e2668fbdf8de617ced1e10c
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-09-15 22:54:16 +0900

    feat: 사용자 profile 컴포넌트 마크업 및 스타일링 추가

--- src/pages/EditUserInfo/components/ChangeUserProfilePic/ChangeUserProfilePic.jsx
+++ src/pages/EditUserInfo/components/ChangeUserProfilePic/ChangeUserProfilePic.jsx
@@ -1,20 +1,24 @@
 import { useId } from 'react';
+import DefaultUserIcon from '@/assets/svg/default-user.svg?react';
 import CameraIcon from '@/assets/svg/camera.svg?react';
+import S from './ChangeUserProfilePic.module.css';
 
 function ChangeUserProfilePic() {
   const id = useId();
   return (
-    <article>
+    <article className={S.component}>
       <h3 className="sr-only">사용자 프로필 사진 변경 영역</h3>
-      <div>
-        <label htmlFor={id}>
-          <span>s</span>
-          <span>
+      <div className={S.userProfileChoicerWrapper}>
+        <label htmlFor={id} className={S.userProfileChoicer}>
+          <span className={S.userProfile}>
+            <DefaultUserIcon aria-label="기본 시용자 사진 아이콘" />
+          </span>
+          <span className={S.cameraIcons}>
             <CameraIcon aria-label="카메라 아이콘" />
           </span>
         </label>
+        <input id={id} type="file" />
       </div>
-      <input id={id} type="file" />
     </article>
   );
 }
