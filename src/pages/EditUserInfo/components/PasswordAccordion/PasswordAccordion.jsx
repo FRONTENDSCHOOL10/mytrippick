5b29eb0c0aedc7d1072e2fa1c162c98687ff0a5e
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-09-16 00:22:20 +0900

    feat: 비밀번호 변경 아코디언 마크업 및 스타일링

--- src/pages/EditUserInfo/components/PasswordAccordion/PasswordAccordion.jsx
+++ src/pages/EditUserInfo/components/PasswordAccordion/PasswordAccordion.jsx
@@ -1,13 +1,18 @@
 import AppInput from '@/components/AppInput/AppInput';
+import ChevronIcon from '@/assets/svg/chevron.svg?react';
 import S from './PasswordAccordion.module.css';
 
 function PasswordAccordion() {
   return (
     <article className={S.component}>
       <h3 className="sr-only">비밀번호 변경을 위한 아코디언</h3>
-      <details>
-        <summary>비밀번호 변경</summary>
-        <div>
+      <details open>
+        <summary>
+          <ChevronIcon className={S.chevronIcon} />
+          비밀번호 변경
+        </summary>
+
+        <div className={S.passwordInputWrapper}>
           <div>
             <AppInput
               label={'새로운 비밀번호'}
@@ -16,7 +21,10 @@ function PasswordAccordion() {
               name={'newPassword'}
               placeholder={'새로운 비밀번호를 입력해주세요'}
             />
-            <span></span>
+            <span className="caption">
+              숫자, 특수문자를 최소 1가지 이상 포함한 대소문자 구분 없는 영문
+              8~15자
+            </span>
           </div>
           <div>
             <AppInput
