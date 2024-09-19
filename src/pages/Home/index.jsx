8c10fd5b37687ab5df4295aa39f053f8d5f0a608
hyesu-jeong <hyeppy739@gmail.com>
2024-09-19 18:05:49 +0900

    Refactor: 홈 랭킹 카드 모달 컴포넌트 추가

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,10 +6,30 @@ import CategoryTab from './components/CategoryTab/CategoryTab';
 import RankList from './components/CardList/RankList';
 import PostList from './components/CardList/PostList';
 
+import BasicTextModal from '@/components/BasicTextModal/BasicTextModal';
+import useModalStore from '@/stores/useModalStore';
+import { useNavigate } from 'react-router-dom';
+import { formatTextWithBreaks } from '@/utils';
+
 import { Helmet } from 'react-helmet-async';
 import S from './Home.module.css';
 
 export function Component() {
+  const showModal = useModalStore((state) => state.showModal);
+  const closeModal = useModalStore((state) => state.closeModal);
+  const navigate = useNavigate();
+
+  // 모달 '로그인' 버튼 클릭 시 로그인 페이지로 이동
+  const handleLoginRedirect = () => {
+    closeModal(); // 모달 닫기
+    navigate('/login'); // 로그인 페이지로 이동
+  };
+
+  // 모달 메시지
+  const message = formatTextWithBreaks(
+    '로그인 이후 이용 가능합니다.\n로그인 페이지로 이동하시겠습니까?'
+  );
+
   return (
     <>
       <Helmet>
@@ -33,6 +53,18 @@ export function Component() {
       <CategoryTab />
       {/* 게시글 리스트 */}
       <PostList />
+
+      {/* 모달 렌더링 */}
+      {showModal && (
+        <BasicTextModal
+          type="both"
+          message={message}
+          fillBtnText="로그인"
+          btnText="닫기"
+          onFillBtnClick={handleLoginRedirect} // 로그인 페이지로 이동
+          onBtnClick={closeModal} // 모달 닫기
+        />
+      )}
     </>
   );
 }
