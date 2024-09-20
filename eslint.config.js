5fe83d182628800528e7101effd7d7b9b3b24023
Oh Seong Hun <102678445+castlehuni@users.noreply.github.com>
2024-09-20 10:34:20 +0900

    feat: 2차 피드백 내용 중 @esline/compact 설치 및 적용

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,18 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-import pluginReactHooks from "eslint-plugin-react-hooks";
-import pluginReactRefresh from "eslint-plugin-react-refresh";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import { fixupPluginRules } from '@eslint/compat';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    ignores: ["dist"],
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    ignores: ['dist'],
     languageOptions: {
       parserOptions: {
-        sourceType: "module",
-        ecmaVersion: "latest",
+        sourceType: 'module',
+        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
       },
       globals: {
@@ -21,23 +22,23 @@ export default [
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     plugins: {
       react: pluginReact,
-      "react-hooks": pluginReactHooks,
-      "react-refresh": pluginReactRefresh,
+      'react-hooks': fixupPluginRules(pluginReactHooks),
+      'react-refresh': fixupPluginRules(pluginReactRefresh),
     },
     rules: {
       ...pluginJs.configs.recommended.rules,
       ...pluginReact.configs.recommended.rules,
-      ...pluginReact.configs["jsx-runtime"].rules,
+      ...pluginReact.configs['jsx-runtime'].rules,
       ...pluginReactHooks.configs.recommended.rules,
-      "react/jsx-no-target-blank": "warn",
-      "react/prop-types": "warn",
-      "react-refresh/only-export-components": [
-        "warn",
+      'react/jsx-no-target-blank': 'warn',
+      'react/prop-types': 'warn',
+      'react-refresh/only-export-components': [
+        'warn',
         { allowConstantExport: true },
       ],
     },
--- src/pages/EditUserInfo/index.jsx
+++ src/pages/EditUserInfo/index.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import {
   checkCommentsMySelfLength,
