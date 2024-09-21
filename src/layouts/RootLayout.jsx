52fd39998fcfa181882fe7a6fccf8e69239523a2
hyesu-jeong <hyeppy739@gmail.com>
2024-09-21 18:23:07 +0900

    Feat: 북마크 핸들러 제작

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -4,6 +4,7 @@ import Header from '@/components/Header/Header';
 import ScrollDirectionDetector from '@/components/ScrollDirectionDetector';
 import { Outlet } from 'react-router-dom';
 import LikesHandler from '@/components/LikesHandler';
+import BookmarkHandler from '@/components/BookmarkHandler';
 
 const queryClient = new QueryClient();
 
@@ -14,6 +15,7 @@ function RootLayout() {
         <Header />
         <ScrollDirectionDetector />
         <LikesHandler />
+        <BookmarkHandler />
         <main className="container">
           <Outlet />
         </main>
