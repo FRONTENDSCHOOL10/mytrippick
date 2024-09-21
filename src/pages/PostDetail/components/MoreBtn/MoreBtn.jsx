e6a17defd8783061d350837c012e1e8a0e0da21c
rewind122 <luvyoudear12@gmail.com>
2024-09-22 02:53:13 +0900

    refactor: 게시글 상세 페이지 기능 추가
    - 게시글 수정 페이지 연결
    - 게시글 삭제 기능 추가
    - 댓글 삭제 기능 추가

--- src/pages/PostDetail/components/MoreBtn/MoreBtn.jsx
+++ src/pages/PostDetail/components/MoreBtn/MoreBtn.jsx
@@ -3,12 +3,15 @@ import clsx from 'clsx';
 import { bool } from 'prop-types';
 import { useRef } from 'react';
 import S from './MoreBtn.module.css';
+import { func } from 'prop-types';
 
 MoreBtn.propTypes = {
   small: bool,
+  onClickEdit: func,
+  onClickDelete: func,
 };
 
-function MoreBtn({ small = false }) {
+function MoreBtn({ small = false, onClickEdit, onClickDelete }) {
   const hiddenBtnRef = useRef();
 
   const btnClass = clsx(S.btn, {
@@ -23,19 +26,16 @@ function MoreBtn({ small = false }) {
     }
   };
 
-  const handleEdit = () => {};
-  const handleDelete = () => {};
-
   return (
     <div role="group" className={S.btnContainer}>
       <button type="button" onClick={handleClick} className={btnClass}>
         <Dots />
       </button>
       <div role="group" ref={hiddenBtnRef} className={S.hiddenBtn}>
-        <button type="button" onClick={handleEdit}>
+        <button type="button" onClick={onClickEdit}>
           수정하기
         </button>
-        <button type="button" onClick={handleDelete}>
+        <button type="button" onClick={onClickDelete}>
           삭제하기
         </button>
       </div>
--- src/pages/PostDetail/components/PostText/PostText.jsx
+++ src/pages/PostDetail/components/PostText/PostText.jsx
@@ -1,6 +1,6 @@
 import Plus from '@/assets/svg/plus.svg?react';
 import { string } from 'prop-types';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import S from './PostText.module.css';
 
 PostText.propTypes = {
@@ -9,26 +9,34 @@ PostText.propTypes = {
 
 function PostText({ children }) {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    setShowButton(children && children.length > 150);
+    setIsExpanded(false);
+  }, [children]);
 
   const handleToggle = () => {
     setIsExpanded(!isExpanded);
   };
 
-  if (children) {
-    return (
-      <section className={S.postText}>
-        <p className="body1">
-          {isExpanded
-            ? children
-            : children.slice(0, 150) + (children.length > 150 ? '...' : '')}
-        </p>
+  if (!children) return null;
+
+  return (
+    <section className={S.postText}>
+      <p className="body1">
+        {isExpanded
+          ? children
+          : children.slice(0, 150) + (showButton ? '...' : '')}
+      </p>
+      {showButton && (
         <button className={S.btn} onClick={handleToggle}>
           {isExpanded ? '접기' : '더보기'}
-          {isExpanded ? '' : <Plus />}
+          {!isExpanded && <Plus />}
         </button>
-      </section>
-    );
-  }
+      )}
+    </section>
+  );
 }
 
 export default PostText;
