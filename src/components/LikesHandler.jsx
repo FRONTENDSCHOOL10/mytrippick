4f91305c1b3873268e7275dfb9f42d510ef39366
hyesu-jeong <hyeppy739@gmail.com>
2024-09-21 17:49:31 +0900

    Update

--- src/components/LikesHandler.jsx
+++ src/components/LikesHandler.jsx
@@ -1,45 +1,47 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import useGlobalStore from '@/stores/useGlobalStore';
-import pb from '@/api/pb';
 
 export default function LikesHandler() {
-  const userId = useGlobalStore((state) => state.currentUserId);
+  const API_URL = import.meta.env.VITE_PB_URL;
+  // const userId = useGlobalStore((state) => state.userId); 유저아이디 undefined
   const likedPostIds = useGlobalStore((state) => state.likedPostIds);
   const setLikedPostIds = useGlobalStore((state) => state.setLikedPostIds);
   const [likesPbId, setLikesPbId] = useState(null);
+  const userId = '3bvc1x2g5ij8asg';
 
-  useQuery({
+  const likedData = useQuery({
     queryKey: ['likes', userId],
-    queryFn: async () => {
-      if (!userId) return null; // userId가 없으면 쿼리 실행하지 않음
-
-      const res = await pb.collection('likes').getList(1, 1, {
-        filter: `userId="${userId}"`,
-      });
-
-      if (res.items && res.items.length > 0) {
-        const likedItem = res.items[0];
-        setLikedPostIds([likedItem.postId]); // 좋아요한 postId를 배열로 설정
-        setLikesPbId(likedItem.id); // 좋아요 기록의 ID 저장
-      }
-
-      return res;
-    },
-    enabled: !!userId, // userId가 있을 때만 쿼리 실행
+    queryFn: () =>
+      axios
+        .get(`${API_URL}/api/collections/likes/records`, {
+          params: {
+            filter: `(userId="${userId}")`,
+          },
+        })
+        .then((res) => {
+          setLikedPostIds(res.data?.items[0].postId);
+          setLikesPbId(res.data?.items[0].id);
+          return res.data;
+        }),
   });
 
   useEffect(() => {
-    if (!likesPbId || likedPostIds?.length <= 0) return;
+    if (!likesPbId || likedPostIds?.length < 0) return;
+    console.log('Updating likes:', likedPostIds);
 
     const updateLikes = async () => {
       try {
-        await pb.collection('likes').update(likesPbId, {
-          postId: likedPostIds, // 업데이트할 postId 보내기
-        });
-        console.log('좋아요 업데이트 성공');
+        await axios.patch(
+          `${API_URL}/api/collections/likes/records/${likesPbId}`,
+          {
+            postId: likedPostIds,
+          }
+        );
+        console.log('Likes updated successfully');
       } catch (error) {
-        console.error('좋아요 업데이트 실패:', error);
+        console.error('Error updating likes:', error);
       }
     };
 
