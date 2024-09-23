489662f36c04c10b2be2c3acdab7685948ae002a
hyesu-jeong <hyeppy739@gmail.com>
2024-09-23 19:01:58 +0900

    Feat: 게시글 최대 15개까지만 들어오게 수정

--- src/pages/Ranking/index.jsx
+++ src/pages/Ranking/index.jsx
@@ -4,7 +4,6 @@ import Card from '@/components/Card/Card';
 import CommonBtn from '@/components/CommonBtn/CommonBtn';
 import BasicTextModal from '@/components/BasicTextModal/BasicTextModal';
 import AppSpinner from '@/components/AppSpinner/AppSpinner';
-import AppHelmet from '@/components/AppHelmet/AppHelmet';
 import { useState, Fragment } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
@@ -13,10 +12,13 @@ import { throttle } from '@/utils';
 import S from './Ranking.module.css';
 import axios from 'axios';
 
+const MAX_POSTS = 15;
+
 export function Component() {
   const [rankCardList, setRankCardList] = useState([]);
   const [page, setPage] = useState(1);
   const [userIds, setUserIds] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
   const likedPostIds = useGlobalStore((state) => state.likedPostIds);
   const bookmarkedPostIds = useGlobalStore((state) => state.bookmarkedPostIds);
   const showModal = useModalStore((state) => state.showModal);
@@ -26,7 +28,9 @@ export function Component() {
 
   // 쓰로틀된 페이지 증가 함수
   const throttledIncreasePage = throttle(() => {
-    setPage((prevPage) => prevPage + 1);
+    if (rankCardList.length < MAX_POSTS) {
+      setPage((prevPage) => prevPage + 1);
+    }
   }, 1000);
 
   const rankData = useQuery({
@@ -38,17 +42,26 @@ export function Component() {
         })
         .then((res) => {
           const newItems = res.data.items || [];
-          setRankCardList((prevList) =>
-            page === 1 ? newItems : [...prevList, ...newItems]
-          );
+          setRankCardList((prevList) => {
+            const updatedList =
+              page === 1 ? newItems : [...prevList, ...newItems];
+            return updatedList.slice(0, MAX_POSTS);
+          });
           const extractedUserIds = newItems.map((item) => item.userId);
-          setUserIds((prevIds) => [...prevIds, ...extractedUserIds]);
-          console.log('추출된 userIds:', extractedUserIds);
+          setUserIds((prevIds) => [
+            ...new Set([...prevIds, ...extractedUserIds]),
+          ]);
+          setHasMore(
+            rankCardList.length + newItems.length <
+              Math.min(res.data.totalItems, MAX_POSTS)
+          );
+          // console.log('추출된 userIds:', extractedUserIds);
 
           return res.data; // 원본 데이터를 그대로 반환
         }),
     keepPreviousData: true, // 이전 데이터를 유지하여 더 나은 사용자 경험 제공
     refetchOnWindowFocus: false,
+    enabled: rankCardList.length < MAX_POSTS,
   });
 
   const userData = useQuery({
@@ -57,7 +70,6 @@ export function Component() {
       axios
         .get(`${API_URL}/api/collections/users/records`, {
           params: {
-            // Construct the filter string dynamically
             filter: userIds.map((id) => `id='${id}'`).join(' || '),
           },
         })
@@ -81,8 +93,8 @@ export function Component() {
 
   // 모달 onFillBtn 버튼 클릭 시 로그인 페이지로 이동
   const handleLoginRedirect = () => {
-    closeModal(); // 모달 닫기
-    navigate('/login'); // 로그인 페이지로 이동
+    closeModal();
+    navigate('/login');
   };
 
   // 모달 메시지
@@ -96,7 +108,6 @@ export function Component() {
 
   return (
     <>
-      <AppHelmet title={'마이트립픽 | 여행지 랭킹 보기'} />
       <h1 className={`headline2 ${S.sectionTitle}`}>인기 여행지 랭킹</h1>
       <section className={S.container}>
         <div className={S.rankCardList}>
@@ -120,12 +131,11 @@ export function Component() {
             </Fragment>
           ))}
         </div>
-        {rankCardList.length > 0 &&
-          rankCardList.length !== rankData.data?.totalItems && (
-            <CommonBtn small onClick={throttledIncreasePage}>
-              더보기
-            </CommonBtn>
-          )}
+        {hasMore && rankCardList.length < MAX_POSTS && (
+          <CommonBtn small onClick={throttledIncreasePage}>
+            더보기
+          </CommonBtn>
+        )}
       </section>
 
       {/* 모달 렌더링 */}
